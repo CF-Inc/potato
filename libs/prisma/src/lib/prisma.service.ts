4d31251eb09266205a41fd164ada6adf1ac47c31
William Sedlacek <wsedlacekc@gmail.com>
2020-08-31 13:50:49 -0700

    feat(prisma): setup prisma library
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

---
+++ libs/prisma/src/lib/prisma.service.ts
@@ -0,0 +1,18 @@
+import {
+  Injectable,
+  OnApplicationBootstrap,
+  OnApplicationShutdown,
+} from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient
+  implements OnApplicationBootstrap, OnApplicationShutdown {
+  async onApplicationBootstrap() {
+    await this.$connect();
+  }
+
+  async onApplicationShutdown() {
+    await this.$disconnect();
+  }
+}
